b9f8e46762ed5dc00ecd6ea581f48ea610bee393
SldTir <54772323+SldTir@users.noreply.github.com>
2020-06-30 01:09:01 +0300

    Доработал загрузку фотографий

--- js/avatar.js
+++ js/avatar.js
@@ -3,9 +3,11 @@
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   var fileChooser = document.querySelector('.ad-form-header__input');
-  var preview = document.querySelector('.ad-form-header__preview').querySelector('img');
+  var previewUser = document.querySelector('.ad-form-header__preview').querySelector('img');
+  var previewHousing = document.querySelector('.ad-form__photo');
+  var fileHousing = document.querySelector('.ad-form__input');
 
-  var pridumatNazvanie = function (evt) {
+  var userAvatar = function (evt) {
     var file = evt.target.files[0];
     var fileName = file.name.toLowerCase();
 
@@ -17,11 +19,32 @@
       var reader = new FileReader();
 
       reader.addEventListener('load', function () {
-        preview.src = reader.result;
+        previewUser.src = reader.result;
       });
       reader.readAsDataURL(file);
     }
   };
 
-  fileChooser.addEventListener('change', pridumatNazvanie);
+  var imageHousing = function (evt) {
+    var file = evt.target.files[0];
+    var fileName = file.name.toLowerCase();
+    var elementImg = previewUser.cloneNode(true)
+    previewHousing.appendChild(elementImg);
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        elementImg.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+    }
+  };
+
+  fileChooser.addEventListener('change', userAvatar);
+  fileHousing.addEventListener('change', imageHousing);
 })();
